1e6392d0e971f2e27f37d513b6e83249dd14d53c
michellejs <michelleskolly@michelles-MacBook-Pro.local>
2016-04-19 21:56:43 -0400

    A bit better
    asynchronicity is currently the bane of my existence. I’m sure
    eventually I will think it is awesome but right now I want to shoot
    stuff :)

--- TwitchTV/script.js
+++ TwitchTV/script.js
@@ -17,7 +17,8 @@ function getStreams(channels) {
   );
   // now I need to transform the pertinent info from both streamData and channelData
   //into an array of TwitchUsers
-
+  console.log(streamData.length);
+  console.log(channelData.length);//tomorrow make sure I return the twitchUsers array instead of streamData
 
   // Return a new promise which will resolve when *all* channels
   // have been queried.
@@ -45,8 +46,7 @@ const channels = [
   "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff",
   "ESL_SC2", "brunofin"
 ];
-var streamData = [];
-var channelData = [];
+
 var twitchUsers = [];
 getStreams(channels).then(
   // infos is an array of resolved data about the channels
@@ -55,7 +55,68 @@ getStreams(channels).then(
     info => console.log(info)
   )
 );
+/*$(document).ready(function() {
+  var channels = [
+    "freecodecamp", "storbeck", "terakilobyte", "habathcx",
+    "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff",
+    "ESL_SC2", "brunofin"
+  ];
 
+  var test = [];
+  var test2 = [];
+
+  function getData() {
+    var promises = [];
+    for (var i = 0; i < channels.length; i++) {
+      promises.push(
+        $.getJSON(
+          'https://api.twitch.tv/kraken/streams/'+channels[i]+'?callback=?'
+        )
+      );
+    }
+
+    return $.when.apply($, promises).then(function() {
+      for(var j=0; j<promises.length; j++){
+        test.push(promises[j]);
+      }
+    });
+  }
+  function getData2() {
+    var promises = [];
+    for (var i = 0; i < channels.length; i++) {
+      promises.push(
+        $.getJSON(
+          'https://api.twitch.tv/kraken/channels/'+channels[i]+'?callback=?'
+        )
+      );
+    }
+
+    return $.when.apply($, promises).then(function() {
+      for(var j=0; j<promises.length; j++){
+        test2.push(promises[j]);
+      }
+    });
+  }
+  function finalTest(e, i, a){
+  		console.log(test[i]);
+  }
+
+  getData().then(function() {
+    console.log("do I have data?: ");
+    for(var j=0; j<test.length; j++) {
+       console.log(test[j].responseJSON);
+     }
+  });
+    getData2().then(function() {
+    console.log("do I have data?: ");
+    for(var j=0; j<test2.length; j++) {
+       console.log(test2[j].responseJSON);
+     }
+  });
+    console.log(test.length);
+    test.forEach(finalTest);
+
+});*/
 
 
 
